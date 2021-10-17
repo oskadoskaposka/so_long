9d2b5a5ee9fb84d9683c3e9644327cdcb43309dc
oskadoskaposka <alepaduanlima@gmail.com>
2021-10-16 22:21:04 -0300

    updating files

--- example_so_long.c
+++ example_so_long.c
@@ -6,14 +6,11 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/03 00:32:37 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/10/06 21:02:57 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/10/16 21:09:40 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/so_long.h"
-
-//apagar
-#include <stdio.h>
 
 int main (void)
 {
@@ -29,8 +26,8 @@ int main (void)
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx,250,50,"Hello World!");
 
-	print_wall = mlx_xpm_file_to_image(mlx, "imgs/parede.xpm", &wall_x, &wall_y);
-	chao = mlx_xpm_file_to_image(mlx, "imgs/teste.xpm", &wall_x, &wall_y);
+	print_wall = mlx_xpm_file_to_image(mlx, "imgs/parede48x40.xpm", &wall_x, &wall_y);
+	chao = mlx_xpm_file_to_image(mlx, "imgs/chao50x50.xpm", &wall_x, &wall_y);
 	
 	mlx_put_image_to_window(mlx, mlx_win,chao, 0, 0);
 	mlx_put_image_to_window(mlx, mlx_win,chao, 50, 0);
@@ -43,9 +40,6 @@ int main (void)
 	mlx_put_image_to_window(mlx, mlx_win,print_wall, 110, 2);
 	mlx_put_image_to_window(mlx, mlx_win,print_wall, 160, 2);
 	mlx_put_image_to_window(mlx, mlx_win,print_wall, 210, 2);
-
-	printf("O tamanho de x é %d \n", wall_x);
-	printf("O tamanho de y é %d \n", wall_y);
 
 	mlx_loop(mlx);
 
