4f6e812685481e642620e1560b601bad4ba11190
oskadoskaposka <alepaduanlima@gmail.com>
2021-10-21 23:50:29 -0300

    use of count lines

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 22:21:54 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/10/21 23:39:59 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/10/21 23:44:28 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,22 @@ int	main(void)
 {
 	int		i;
 	char	*s;
-	int		fd;
-	// t_map	map;
-	char **map;
-
-	char *path_map;
+	t_map	map;
 
 
 	i = 0;
 	s = "teste";
-	fd = open("src/map.ber", O_RDONLY);
- 	map = malloc((5) * sizeof(char *));
+	map.path_map = "src/map.ber";
 
-	path_map = "src/map.ber";
+	count_lines(map.path_map);
 
-	count_lines(path_map);
-
-	while (s)
-	{
-		s = get_next_line(fd);
-		if (s)
-		{
-			printf("%s", s);
-			map[i] = ft_strdup(s); 
-		}
-		i++;
-	}
+	// while (s)
+	// {
+	// 	s = get_next_line(fd);
+	// 	if (s)
+	// 		printf("%s", s);
+	// 	i++;
+	// }
 	printf ("\n\n%i \n", i);
 	return (0);
 }
