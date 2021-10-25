010909ca220a0cedd3634187703bcbee53fa38a3
oskadoskaposka <alepaduanlima@gmail.com>
2021-10-24 23:31:04 -0300

    printing floor in all tiles

--- src/2_window/ft_print_window.c
+++ src/2_window/ft_print_window.c
@@ -6,7 +6,7 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 03:12:51 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/10/24 23:21:34 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/10/24 23:31:03 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,7 @@ void	ft_print_window(t_game *game)
 			mlx_put_image_to_window(game->mlx.ptr, game->mlx.ptr_win, game->img.floor.img, (i * 50), (j * 50));
 			i++;
 		}
-		printf("Imprimiu %i vezes o i\n", i);
 		i = 0;
 		j++;
 	}
-	printf("\nImprimiu %i vezes o j\n", j);
 }
